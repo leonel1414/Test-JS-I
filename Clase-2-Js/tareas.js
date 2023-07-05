a9179971198ec360e738bf8648860d09b2c0b86e
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-05 19:45:05 -0300

    Modificaciones

--- Clase-2-Js/tareas.js
+++ Clase-2-Js/tareas.js
@@ -117,7 +117,7 @@ function elevarAlCuadrado(num) {
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  return num **3;
+  return num ** 3;
 }
 
 function elevar(num, exponent) {
