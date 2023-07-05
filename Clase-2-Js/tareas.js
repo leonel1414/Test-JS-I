3314d7c84c4c34af15c4173c6a989e3989f2e306
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-05 19:46:30 -0300

    Modificaciones

--- Clase-2-Js/tareas.js
+++ Clase-2-Js/tareas.js
@@ -200,6 +200,7 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado) {
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
+  return lado * 4;
 }
 
 function areaDelTriangulo(base, altura) {
