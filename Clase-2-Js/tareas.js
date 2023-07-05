e10a419894bfe30c838a506b7e2e39eb592473e8
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-05 20:49:41 -0300

    Modificaciones

--- Clase-2-Js/tareas.js
+++ Clase-2-Js/tareas.js
@@ -164,7 +164,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return str+"!";
+  return str + "!";
 }
 
 function combinarNombres(nombre, apellido) {
