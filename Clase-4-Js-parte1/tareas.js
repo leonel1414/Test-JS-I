7768e1bd5bf1136dfdd332a788147cefa4e50e20
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-08-01 19:19:05 -0300

    Modificaciones

--- Clase-4-Js-parte1/tareas.js
+++ Clase-4-Js-parte1/tareas.js
@@ -3,16 +3,19 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+   return array[0];
 }
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[length-1];
 }
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array[arr.length-1];
 }
 
 function incrementarPorUno(array) {
