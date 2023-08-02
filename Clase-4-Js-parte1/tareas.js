948149f43dca4f165245e59a6232e7ca3247bf32
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-08-01 23:43:21 -0300

    Ultimo ajustes

--- Clase-4-Js-parte1/tareas.js
+++ Clase-4-Js-parte1/tareas.js
@@ -179,6 +179,20 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  let mesesBuscados = ["Enero", "Marzo", "Noviembre"];
+  let mesesEncontrados = [];
+
+  for(i = 0; i< array.length; i++){
+    if(mesesBuscados.includes(array[i])){
+      mesesEncontrados.push(array[i]);
+    }
+  }
+
+  if (mesesEncontrados.length === 3) {
+    return mesesEncontrados;
+  } else {
+    return "No se encontraron los meses pedidos";
+  }
 }
 
 function mayorACien(array) {
