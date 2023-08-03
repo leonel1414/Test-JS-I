d44a42c78e682cce655c4bafabbec33214cc530c
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-08-02 23:08:37 -0300

    ok terminado

--- Clase-4-Js-parte2/tareas.js
+++ Clase-4-Js-parte2/tareas.js
@@ -152,6 +152,12 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+
+  producto.calcularPrecioDescuento = function() {
+    return producto.precio - (producto.precio * producto.porcentajeDeDescuento);
+  };
+
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
