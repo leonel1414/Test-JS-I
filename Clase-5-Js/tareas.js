e24283400ad46adfea5ac845e4e0bb4bfec58b41
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-08-07 21:22:19 -0300

    Ok terminado

--- Clase-5-Js/tareas.js
+++ Clase-5-Js/tareas.js
@@ -7,10 +7,13 @@ function counter() {
   // ejemplo: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let contador = 0;
+  return () => ++contador;
 }
 
 function cacheFunction(cb) {
-  // Usa closures para crear un caché para la función cb.
+
+   // Usa closures para crear un caché para la función cb.
   // la función que retornas debe aceptar un solo argumento e invocar a cb con ese argumento
   // cuando la función que hayas retornado es invocada de nuevo, debería guardar el argumento y el resultado de la invocacion
   // cuando la función que retornaste sea invocada de nuevo con un argumento con el cual se había invocado anterioremente, no deberia invocar de nuevo a cb
@@ -21,8 +24,27 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
-}
 
+  
+  // Creamos el objeto para almacenar los resultados en caché
+  const cache = {};
+
+  // Retornamos la función que acepta un solo argumento
+  return function (arg) {
+    // Verificamos si el resultado ya está en caché
+    if (cache.hasOwnProperty(arg)) {
+      // Si está en caché, lo retornamos directamente
+      return cache[arg];
+    } else {
+      // Si no está en caché, invocamos a cb con el argumento dado
+      const result = cb(arg);
+      // Guardamos el resultado en caché
+      cache[arg] = result;
+      // Retornamos el resultado
+      return result;
+    }
+  };
+}
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
